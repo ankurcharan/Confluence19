19079fe2524d6f68730ecc66de1500b643b41365
Ankur Charan <ankurcharan98@gmail.com>
2018-12-18 22:29:25 +0530

    adding navbar and footer
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+	render() {
+		return (
+			<BrowserRouter>
 
-export default App;
+				<div>
+					<Nav />
+					<br />
+					<br />
+					<Footer />
+				</div>
+
+			</BrowserRouter>
+			);
+		}
+	}
+	
+	export default App;
+	
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Nav.css';
+
+class Nav extends React.Component {
+
+    render() {
+
+        return (
+
+
+            <nav className="teal" role="navigation">
+                <div className="nav-wrapper container">
+                    <Link to='/'>
+                        <a id="logo-container" href="/" className="brand-logo">
+                            <i id="logoIcon" className="large material-icons">
+                                developer_mode
+                            </i>
+                            Confluence
+                        </a>
+                    </Link>
+                    <ul class="right hide-on-med-and-down">
+                        <li>
+                            <Link to='/'>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/events'>
+                                Events
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/gallery'>
+                                Gallery
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/sponsors'>
+                                Sponsors
+                            </Link>
+                        </li>
+                    </ul>
+                    
+                    <ul id="nav-mobile" class="sidenav">
+                    <li>
+                            <Link to='/'>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/events'>
+                                Events
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/gallery'>
+                                Gallery
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/sponsors'>
+                                Sponsors
+                            </Link>
+                        </li>
+                    </ul>
+                    
+                    <a href='#!' data-target="nav-mobile" class="sidenav-trigger"><i id='phoneIcon' class="material-icons">menu</i></a>
+                </div>
+            </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            // <div>
+            //     <nav>
+            //         <div class="nav-wrapper">
+            //             <a href="/" class="brand-logo">
+            //                 Conflu
+            //             </a>
+            //             <a href="#!" data-target="mobile-demo" class="sidenav-trigger">
+            //                 <i class="material-icons">
+            //                     menu
+            //                 </i>
+            //             </a>
+            //             <ul class="right hide-on-med-and-down">
+            //                 <li>
+            //                     <Link to='/'>
+            //                         Home
+            //                     </Link>    
+            //                 </li>
+            //                 <li>
+            //                     <Link to='/events'>
+            //                         Events
+            //                     </Link>    
+            //                 </li>
+            //                 <li>
+            //                     <Link to='/gallery'>
+            //                         Gallery
+            //                     </Link>    
+            //                 </li>
+            //                 <li>
+            //                     <Link to='/sponsors'>
+            //                         Sponsors
+            //                     </Link>    
+            //                 </li>
+            //             </ul>
+            //         </div>
+            //     </nav>
+
+            //     <ul class="sidenav" id="mobile-demo">
+            //         <li>
+            //             <Link to='/'>
+            //                 Home
+            //             </Link>    
+            //         </li>
+            //         <li>
+            //             <Link to='/events'>
+            //                 Events
+            //             </Link>    
+            //         </li>
+            //         <li>
+            //             <Link to='/gallery'>
+            //                 Gallery
+            //             </Link>    
+            //         </li>
+            //         <li>
+            //             <Link to='/sponsors'>
+            //                 Sponsors
+            //             </Link>    
+            //         </li>
+            //     </ul>
+            // </div>
+                    
+
+        );
+    }
+}
+
+export default Nav;
